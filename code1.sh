0ea3b8196520fb0be6a2b1802d3dd3f326ae56fa
sourav <abc@mail.com>
2021-04-14 03:11:41 +0000

    sourav file before stash

--- code1.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-a=1
-echo "The value of a is $a"
--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Hellow World"
-echo "Hellow Shyamnagar"
-x=5
-echo "$x"
-echo "from master branch"
-echo "Hellow new branch"
